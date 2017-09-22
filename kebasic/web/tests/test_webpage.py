2197ad174f375bae5afc8db7b05af4113ac152b6
Cezar Sas <cezar.sas@gmail.com>
2017-09-22 09:01:49 +0200

    Comments

--- kebasic/web/tests/test_webpage.py
+++ kebasic/web/tests/test_webpage.py
@@ -17,6 +17,10 @@ abogados_html.parse()
 
 class TestWebPage(unittest.TestCase):
     def test_download(self):
+        """
+        Tests if the class downloads all and the same content contained in the page.
+        :return:
+        """
         cleaned_abogados_html = clean(abogados_html.html).replace(" ", "")
 
         with open("abogadosvilagarcia.html", "rt", encoding="utf8") as inf:
@@ -25,9 +29,17 @@ class TestWebPage(unittest.TestCase):
 
         similarity = SequenceMatcher(None, cleaned_abogados_html, url_source).ratio()
         logging.info("HTML Similarity: {}".format(similarity))
+
+        # Threshold denominated using euristics
+        # Differences are due to order of attributes
         self.assertTrue(similarity > 0.85)
 
     def test_text_extraction(self):
+        """
+        Test the result of visible test extracted by the class
+
+        :return:
+        """
         abogados_text = clean(abogados_html.text)
 
         with open("abogadosvilagarcia.txt", "rt", encoding="utf8") as inf:
@@ -36,6 +48,8 @@ class TestWebPage(unittest.TestCase):
 
         similarity = SequenceMatcher(None, abogados_text.lower(), visible_text.lower()).ratio()
         logging.info("Visible Text Similarity: {}".format(similarity))
+        # Threshold denominated using euristics
+        # Differences are due to spaces and minor chars
         self.assertTrue(similarity > 0.95)
 
     """
