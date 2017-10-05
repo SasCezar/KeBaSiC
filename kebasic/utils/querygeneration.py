ee3d1e823a258f51250e26404ee8c512ee763777
Cezar Sas <cezar.sas@gmail.com>
2017-10-05 10:39:09 +0200

    Code commented

--- kebasic/utils/querygeneration.py
+++ kebasic/utils/querygeneration.py
@@ -3,6 +3,12 @@ import os
 
 
 def read_dict_csv(file_path):
+    """
+    Reads the keywords in the file as a dictionary
+
+    :param file_path:
+    :return:
+    """
     if not file_path:
         return None
     with open(file_path, "rt", encoding="utf8") as inf:
@@ -13,23 +19,37 @@ def read_dict_csv(file_path):
 
 
 def generate_queries(template_path, out_path, keywords_path=None, sites_path=None):
+    """
+    Generates a file containing the queries that will be used to scrape a search engine
+
+    :param template_path:
+    :param out_path:
+    :param keywords_path:
+    :param sites_path:
+    :return:
+    """
     query_generator = QueryGenerator(template_path)
-
     keywords = read_dict_csv(keywords_path)
-
     sites = read_dict_csv(sites_path)
-
     queries = query_generator.generate_queries(keywords=keywords, sites=sites)
-
     query_generator.save_queries(out_path, queries)
 
 
 class QueryGenerator(object):
+    """
+    Defines a query generator based on templates strings
+    """
     def __init__(self, template_path):
         self._site_templates, self._query_templates = self._read_template(template_path)
 
     @staticmethod
     def _read_template(template_path):
+        """
+        Loads the template and separates sites templates and keywords templates
+
+        :param template_path:
+        :return:
+        """
         site_templates = []
         query_templates = []
         with open(template_path, "rt", encoding="utf8") as inf:
@@ -40,6 +60,13 @@ class QueryGenerator(object):
         return site_templates, query_templates
 
     def generate_queries(self, keywords=None, sites=None):
+        """
+        Given two list of strings, returns a list of unique queries based on the class templates
+
+        :param keywords:
+        :param sites:
+        :return:
+        """
         queries = set()
         for keyword in keywords:
             keyword_queries = self._generate_query(keyword, self._query_templates)
@@ -53,6 +80,12 @@ class QueryGenerator(object):
 
     @staticmethod
     def _generate_query(keyword, templates):
+        """
+        Given a keyword and a list of templates returns a list of formatted queries
+        :param keyword:
+        :param templates:
+        :return:
+        """
         queries = set()
         for template in templates:
             query = template.format(**keyword).strip()
@@ -62,5 +95,12 @@ class QueryGenerator(object):
 
     @staticmethod
     def save_queries(out_path, queries):
+        """
+        Saves the queries to file
+
+        :param out_path:
+        :param queries:
+        :return:
+        """
         with open(out_path, "wt", encoding="utf8", newline='') as inf:
             inf.write(os.linesep.join(queries))
