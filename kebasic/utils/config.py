d1278bb7cebb01219031e73a19dc015cc546881c
Cezar Sas <cezar.sas@gmail.com>
2017-12-16 11:13:46 +0100

    Changed from utils to config file

--- kebasic/utils/config.py
+++ kebasic/utils/config.py
@@ -1,12 +1,6 @@
 import json
-from re import finditer
 
 from jsmin import jsmin
-
-
-def camel_case_split(identifier):
-    matches = finditer('.+?(?:(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])|$)', identifier)
-    return " ".join([m.group(0) for m in matches])
 
 
 def load_configs(config_file):
@@ -15,4 +9,3 @@ def load_configs(config_file):
         configs = json.loads(minified)
 
     return configs
-
