705e610b60f1fde2746491a357d1149ad013e62e
Cezar Sas <cezar.sas@gmail.com>
2017-12-16 11:10:50 +0100

    Added normalization

--- kebasic/feature/resultsjoin.py
+++ kebasic/feature/resultsjoin.py
@@ -17,7 +17,12 @@ class SumResults(AbstractScoresCombination):
             for key, score in scores[alg]:
                 results[key.lower()] = score + results[key.lower()] if key.lower() in results else score
 
-        return sorted(results.items(), key=lambda x: (-x[1], x[0]))
+        keywords = results.items()
+        max_score = max([x for _, x in keywords])
+        max_score = max_score if max_score else 1
+
+        keywords = [(kw, score / max_score) for kw, score in keywords]
+        return sorted(keywords, key=lambda x: (-x[1], x[0]))
 
 
 class WeightedSum(AbstractScoresCombination):
