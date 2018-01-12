f0c4f6862c335d380261ca7f1561a198eea2462b
Cezar Sas <cezar.sas@gmail.com>
2018-01-12 23:18:55 +0100

    Added Mongo reader and CSV catalogacion reader

--- kebasic/datasources/webpagedao.py
+++ kebasic/datasources/webpagedao.py
@@ -2,33 +2,69 @@ import csv
 import logging
 from abc import ABC, abstractmethod
 
+from mongoengine import *
+
+from datasources.mongoobjects import WebPage
 from domain.webpagebuilder import WebPageBuilder
 
+connect('kebasic')
 
-class WebPageDAO(ABC):
-    @abstractmethod
+
+class WebPageReader(ABC):
+    def __init__(self, path):
+        self._file = path
+        self._builder = WebPageBuilder()
+
     def load_webpages(self):
+        return self._load_webpages()
+
+    @abstractmethod
+    def _load_webpages(self):
         pass
 
 
-class CSVWebPageDAO(WebPageDAO):
-    def __init__(self, path):
-        self.file = path
-        self.builder = WebPageBuilder()
-
+class CSVWebPageReader(WebPageReader):
     def _load_webpages(self):
-        with open(self.file, "rt", encoding="utf-8-sig") as inf:
+        with open(self._file, "rt", encoding="utf-8-sig") as inf:
             reader = csv.reader(inf)
             for line in reader:
                 url = line[0]
                 html = line[1] if len(line) > 1 else None
                 try:
-                    webpage = self.builder.build(url, html)
+                    webpage = self._builder.build(url, html)
                     yield webpage
                 except Exception as e:
                     logging.error(e)
                     continue
 
-    def load_webpages(self):
-        webpages = list(self._load_webpages())
-        return webpages
+
+class MongoWebPageReader(WebPageReader):
+    def _load_webpages(self):
+        for webpage in WebPage.objects()[:100]:
+            yield webpage
+
+
+class CSVCatalogactionReader(WebPageReader):
+    def __init__(self, path, ontology):
+        super().__init__(path)
+        self._ontology = ontology
+
+    def _load_webpages(self):
+        with open(self._file, "rt", encoding="utf8") as inf:
+            reader = csv.reader(inf)
+            next(inf)
+            for line in reader:
+                url = line[0]
+                category = (line[3] + " " + line[4]).strip()
+
+                category = self._ontology[category]
+
+                page = {"url": url}
+                page.update(category)
+
+                try:
+                    webpage = self._builder.build(**page)
+                except:
+                    continue
+
+                yield webpage
