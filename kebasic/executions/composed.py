d90a41cae60336d35fdc32f4fe059924ad3b306a
marcopalmy <m.palmiero5@campus.unimib.it>
2018-09-13 12:07:45 +0200

    Models ES and EN added

--- kebasic/executions/composed.py
+++ kebasic/executions/composed.py
@@ -38,8 +38,8 @@ class KeywordsExecution(AbstractExecutor):
         builder = WebPageBuilder()
         punctuation_cleaner = PunctuationCleaner()
         jvm.start(packages=True)
-        #wekaclass = WEKAClassifier(self._configs['lvl1_model'], language=self._configs['language'])
-        #wekaclass2 = WEKAClassifier(self._configs['lvl2_model'], language=self._configs['language'])
+        wekaclass = WEKAClassifier(self._configs['lvl1_model'], language=self._configs['language'])
+        wekaclass2 = WEKAClassifier(self._configs['lvl2_model'], language=self._configs['language'])
         out_filename = self._configs['out_path']
         with writer(out_filename, self._configs['std_out']) as outf:
             for json_webpage in webpages:
